886e1b1dd8d9329881257326e62b3f3e0a43b07b
Carlos Albuquerque <carlosphelipe_13@hotmail.com>
2026-08-06 10:36:10 -0300

    Project Created

---
+++ src/test/java/com/carlosalbuquerque/workshopmongo/WorkshopmongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.carlosalbuquerque.workshopmongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopmongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
