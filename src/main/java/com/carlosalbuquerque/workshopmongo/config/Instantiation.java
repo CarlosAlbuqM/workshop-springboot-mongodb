44ae93c5f5e1574c5041e2d2a0d50adbb315f5e0
Carlos Albuquerque <carlosphelipe_13@hotmail.com>
2026-08-07 10:48:49 -0300

    Operação de Instanciação da base de dados

---
+++ src/main/java/com/carlosalbuquerque/workshopmongo/config/Instantiation.java
@@ -0,0 +1,34 @@
+package com.carlosalbuquerque.workshopmongo.config;
+
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+
+import com.carlosalbuquerque.workshopmongo.domain.User;
+import com.carlosalbuquerque.workshopmongo.repository.UserRepository;
+
+@Configuration
+public class Instantiation implements CommandLineRunner{
+
+    @Autowired UserRepository userRepository;
+
+    @Override
+    public void run(String... arg0) throws Exception {
+        
+
+        System.out.println(">>> INSTANCIANDO DADOS <<<");
+        
+        userRepository.deleteAll();
+
+        User maria = new User(null, "Maria Brown", "maria@gmail.com");
+        User alex = new User(null, "Alex Green", "alex@gmail.com");
+        User bob = new User(null, "Bob Grey", "bob@gmail.com");
+
+        userRepository.saveAll(Arrays.asList(maria, alex, bob));
+
+
+    }
+
+}
