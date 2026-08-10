97f1d918834c67a148c2f2a63eb15bf99b087604
Carlos Albuquerque <carlosphelipe_13@hotmail.com>
2026-08-10 10:38:08 -0300

    Insercao de usuario com POST

--- src/main/java/com/carlosalbuquerque/workshopmongo/services/UserService.java
+++ src/main/java/com/carlosalbuquerque/workshopmongo/services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.carlosalbuquerque.workshopmongo.domain.User;
+import com.carlosalbuquerque.workshopmongo.dto.UserDTO;
 import com.carlosalbuquerque.workshopmongo.repository.UserRepository;
 import com.carlosalbuquerque.workshopmongo.services.service.exception.ObjectNotFoundException;
 
@@ -26,6 +27,12 @@ public class UserService {
     Optional<User> obj = repo.findById(id);
         return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
 }
-    
+public User insert(User obj) {
+    return repo.insert(obj);
+}
+
+public User fromDTO(UserDTO objDto) {
+    return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
+}
 
 }
