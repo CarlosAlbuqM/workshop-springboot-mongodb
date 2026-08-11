37d43182b5150fb67d9913844e1b3601934606aa
Carlos Albuquerque <carlosphelipe_13@hotmail.com>
2026-08-11 10:08:32 -0300

    Obtendo um post por ID

---
+++ src/main/java/com/carlosalbuquerque/workshopmongo/services/PostService.java
@@ -0,0 +1,23 @@
+package com.carlosalbuquerque.workshopmongo.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.carlosalbuquerque.workshopmongo.domain.Post;
+import com.carlosalbuquerque.workshopmongo.repository.PostRepository;
+import com.carlosalbuquerque.workshopmongo.services.service.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+
+    @Autowired
+    private PostRepository repo;
+
+    public Post findById(String id) {
+    Optional<Post> obj = repo.findById(id);
+        return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+}
+
+}
